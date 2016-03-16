63b52642769a41add5207ec2a9322a8683dd2898
Gandhi Anderson <thegands@gmail.com>
2016-03-16 17:55:35 -0600

    Cleanup html whitespace

--- app/models/concerns/created_to_s.rb
+++ app/models/concerns/created_to_s.rb
@@ -1,10 +1,14 @@
 module CreatedToS
   def created_to_s
     diff = TimeDifference.between(Time.now, self.created_at).in_general.detect { |k,v| v != 0 }
-    if diff.last == 1
-      "1 #{diff.first.to_s.chop}"
+    if diff
+      if diff.last == 1
+        "1 #{diff.first.to_s.chop}"
+      else
+        "#{diff.last} #{diff.first}"
+      end
     else
-      "#{diff.last} #{diff.first}"
+      "0 seconds"
     end
   end
 end
